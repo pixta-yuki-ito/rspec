c5294f88ea50b531736a4cdecf2492c082757558
pixta-yuki-ito <yuki.ito@pixta.co.jp>
2015-11-16 13:11:31 +0900

    Add spec for ng_word

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -10,6 +10,10 @@ describe MessageFilter do
     subject { MessageFilter.new('foo') }
 
     it_behaves_like 'MessageFilter with argument "foo"'
+
+    it 'ng_words should not be empty' do
+      expect(subject.ng_words.empty?).to eq false
+    end
   end
 
   context 'with argument "foo"' do
