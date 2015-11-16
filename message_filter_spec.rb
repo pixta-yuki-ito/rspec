b056f442570962106b5218968d6fc16648ffdaaa
pixta-yuki-ito <yuki.ito@pixta.co.jp>
2015-11-16 15:11:08 +0900

    'Add example for 2 ng_words

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,18 +1,19 @@
 require_relative 'message_filter'
 require 'rspec/its'
+require 'rspec/collection_matchers'
 
 describe MessageFilter do
   shared_examples 'MessageFilter with argument "foo"' do
     it { is_expected.to be_detect('hello from foo') }
     it { is_expected.not_to be_detect('hello, world!') }
+    its(:ng_words) { is_expected.not_to be_empty }
   end
 
   context 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
 
     it_behaves_like 'MessageFilter with argument "foo"'
-
-    its(:ng_words) { is_expected.not_to be_empty }
+    it { is_expected.to have(1).ng_words }
   end
 
   context 'with argument "foo"' do
@@ -20,5 +21,6 @@ describe MessageFilter do
 
     it { is_expected.to be_detect('hello from bar') }
     it_behaves_like 'MessageFilter with argument "foo"'
+    it { is_expected.to have(2).ng_words }
   end
 end
