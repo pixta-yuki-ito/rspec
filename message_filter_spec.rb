f0be78637d1af190d99ced02d74f53bee3369f0f
pixta-yuki-ito <yuki.ito@pixta.co.jp>
2015-11-16 13:02:59 +0900

    Nest describe blocks

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,20 +1,21 @@
 require_relative 'message_filter'
 
-shared_examples 'MessageFilter with argument "foo"' do
-  it { is_expected.to be_detect('hello from foo') }
-  it { is_expected.not_to be_detect('hello, world!') }
-end
+describe MessageFilter do
+  shared_examples 'MessageFilter with argument "foo"' do
+    it { is_expected.to be_detect('hello from foo') }
+    it { is_expected.not_to be_detect('hello, world!') }
+  end
 
+  describe MessageFilter, 'with argument "foo"' do
+    subject { MessageFilter.new('foo') }
 
-describe MessageFilter, 'with argument "foo"' do
-  subject { MessageFilter.new('foo') }
+    it_behaves_like 'MessageFilter with argument "foo"'
+  end
 
-  it_behaves_like 'MessageFilter with argument "foo"'
-end
+  describe MessageFilter, 'with argument "foo"' do
+    subject { MessageFilter.new('foo', 'bar') }
 
-describe MessageFilter, 'with argument "foo"' do
-  subject { MessageFilter.new('foo', 'bar') }
-
-  it { is_expected.to be_detect('hello from bar') }
-  it_behaves_like 'MessageFilter with argument "foo"'
+    it { is_expected.to be_detect('hello from bar') }
+    it_behaves_like 'MessageFilter with argument "foo"'
+  end
 end
